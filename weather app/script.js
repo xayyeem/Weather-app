16c7875c98e8438dc1d6b1d0ad9dacd1ef0a566c
xayyeem <121686427+xayyeem@users.noreply.github.com>
2023-05-01 22:16:10 +0530

    Add files via upload

---
+++ weather app/script.js
@@ -0,0 +1,24 @@
+const apiKey = "c910e4b8d69233074958e0a3d23f6b87";
+const apiUrl="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const searchBox = document.querySelector(".search input")
+const searchBtn = document.querySelector(".search button")
+
+async function checkWeather(city){
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+    var data = await response.json();
+
+    console.log(data);
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp)+"°c";
+    document.querySelector(".humidity").innerHTML = data.main.humidity+"%";
+    document.querySelector(".wind").innerHTML = data.wind.speed+"km/h";
+
+
+
+}
+
+searchBtn.addEventListener("click",()=>{
+    checkWeather(searchBox.value);
+})
+
+
